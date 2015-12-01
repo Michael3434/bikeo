677523b5e67a7303c275bd7eb7175f5ea2fe7978
Rémy Lombard <rem_lombard@hotmail.com>
2015-12-01 15:48:00 +0100

    correction sur le model bike

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,20 +1,20 @@
 class Bike < ActiveRecord::Base
-  validate :user_id, presence:true
-  validate :type, presence:true
-  validate :size, presence:true
-  validate :accessories, presence:true
-  validate :title, presence:true
-  validate :description, presence:true
-  validate :price_hour, presence:true
-  validate :price_day, presence:true
-  validate :price_week, presence:true
-  validate :price_month, presence:true
-  validate :price_year, presence:true
-  validate :country, presence:true
-  validate :city, presence:true
-  validate :address, presence:true
-  validate :zipcode, presence:true
-  validate :state, presence:true
+  validates :user_id, presence:true
+  validates :type, presence:true
+  validates :size, presence:true
+  validates :accessories, presence:true
+  validates :title, presence:true
+  validates :description, presence:true
+  validates :price_hour, presence:true
+  validates :price_day, presence:true
+  validates :price_week, presence:true
+  validates :price_month, presence:true
+  validates :price_year, presence:true
+  validates :country, presence:true
+  validates :city, presence:true
+  validates :address, presence:true
+  validates :zipcode, presence:true
+  validates :state, presence:true
 
   belongs_to :user
 
