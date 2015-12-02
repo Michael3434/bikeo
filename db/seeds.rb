736ada352d81e422b06d7d621103ac073b7458ef
Alexis Danan <lxsdanan@gmail.com>
2015-12-02 10:36:36 +0100

    problem with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,4 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-9.times do
-  Bike.create(
-    user_id: rand(1..3),
-    category: "momo",
-    size: "big",
-    accessories: "locker",
-    title: "French bike",
-    description: "beau vélo tout neuf",
-    price_hour: 10,
-    price_day: 30,
-    price_week: 150,
-    price_month: 250,
-    price_year: 1000,
-    country: "France",
-    city: "Paris",
-    address: "3 rue du Four",
-    zipcode: 75003,
-    state: "ici"
-    )
-end
+#   Mayor.create(name: 'Emanuel', city: cities.
