fddd367e6e1ae85dfb137cc686c798cbcd542765
Alexis Danan <lxsdanan@gmail.com>
2015-12-03 10:12:34 +0100

    show and index in process

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -3,8 +3,13 @@ class BikesController < ApplicationController
   def index
     @bikes = Bike.all
   end
+
   def new
   bike = Bike.new
+  end
+
+  def show
+    @bike = Bike.find.where(params[:id])
   end
 
 end
